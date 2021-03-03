d7dafed4cfcb13f637d5754fb3cc665ba599fe11
ahmedBassell <ahmedbassell@gmail.com>
2021-03-03 10:20:44 +0200

    Fail tests

--- test/channels/application_cable/connection_test.rb
+++ test/channels/application_cable/connection_test.rb
@@ -3,9 +3,13 @@ require "test_helper"
 class ApplicationCable::ConnectionTest < ActionCable::Connection::TestCase
   # test "connects with cookies" do
   #   cookies.signed[:user_id] = 42
-  #
+  
   #   connect
-  #
+  
   #   assert_equal connection.user_id, "42"
   # end
+
+  test "Fake test" do
+    assert_equal 0, "42"
+  end
 end
